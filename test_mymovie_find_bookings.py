d845ed6627074c55c730b893ffab5a331ee11b0a
Hanan Wiener <hanan888@gmail.com>
2020-02-03 17:22:34 +0200

    master: improved tester that writes log results

--- test_mymovie_find_bookings.py
+++ test_mymovie_find_bookings.py
@@ -1,27 +1,61 @@
 from unittest import TestCase
 from mymovie_find_bookings import *
 import time
-import inspect
 from mymovie_create_input import *
 import logging
 import os
 import yaml
+import copy
 
 
 class TestBidCalculator(TestCase):
+    def _log_test_result(self,
+                         start_time, end_time, total_duration,
+                         gen_input_duration, read_duration, process_duration,
+                         config,
+                         result_num_group, result_total_users, result_total_money,
+                         out_file):
+        test_columns = ['test_name','start_time', 'end_time', 'total_duration',
+                        'gen_input_duration', 'read_input_duration',
+                        'process_duration',
+                        'total_users','user_interest_rows',
+                        'total_titles', 'user_timeslot_rows',
+                        'min_bid', 'max_bid', 'max_num_ticket',
+                        'min_group_bid',
+                        'result_num_group', 'result_total_users',
+                        'result_total_money',
+                        ]
+        test_result_filename = out_file
+        if not os.path.exists(test_result_filename):
+            with open(test_result_filename, 'w') as fdnew:
+                fdnew.write(','.join(test_columns) + '\n')
+        result_string = ','.join([
+            str(config['test_name']),
+            str(int(start_time)), str(int(end_time)), str(int(total_duration)),
+            str(gen_input_duration), str(read_duration), str(process_duration),
+            str(config['total_users']),
+            str(config['user_interest_rows']),
+            str(config['total_titles']),
+            str(config['user_timeslot_rows']),
+            str(config['min_bid'] if 'min_bid' in config else config['fixed_bid']),
+            str(config['max_bid'] if 'max_bid' in config else config['fixed_bid']),
+            str(config['max_num_ticket'] if 'max_num_ticket' in config else 1),
+            str(config['min_group_bid']),
+            str(result_num_group), str(result_total_users), str(result_total_money)
+        ])
+        with open(test_result_filename, 'a') as fda:
+            fda.write(result_string)
+            fda.write('\n')
+
     def setUp(self):
-        self.startTime = time.time()
+        pass
 
     def tearDown(self):
-        t = time.time() - self.startTime
-        print('%s: %.3f' % (self.id(), t))
+        pass
 
     def read_input(self):
         self._interest = read_user_interest()
         self._timeslot = read_timeslot()
-        t = time.time() - self.startTime
-        print('%s: %.3f' % (inspect.currentframe().f_code.co_name, t))
-        self.startTime = time.time()
 
     def _gen_input(self,
                    total_users,
@@ -31,34 +65,80 @@ class TestBidCalculator(TestCase):
                    fixed_bid):
         db = SampleDB(num_user=total_users, num_title=total_titles)
         create_file('user_interest.csv', user_interest_rows,
-                    UserInterestEntryMaker(db, fixed_bid))
+                    UserInterestEntryMaker(db,
+                                           single_ticket_bid_low=fixed_bid,
+                                           single_ticket_bid_high=fixed_bid))
         create_file('user_timeslot.csv', user_timeslot_rows,
                     CreateTimeSlotEntry(db))
-        t = time.time() - self.startTime
-        print('%s: %.3f' % (inspect.currentframe().f_code.co_name, t))
-        self.startTime = time.time()
 
-    def test_(self):
+    # def test_variable_availability(self):
+    #     base_config = {
+    #         'total_users': 2000,
+    #         'user_interest_rows': 4000,
+    #         'total_titles': 30,
+    #         'user_timeslot_rows': -1,
+    #         'fixed_bid': 10,
+    #         'min_group_bid': 300,
+    #         'test_name': 'standard'
+    #     }
+    #     user_timeslot_params = []
+
+    def test_num_users(self):
+        base_config = {
+            'total_users': -1,
+            'user_interest_rows': -1,
+            'total_titles': 5,
+            'user_timeslot_rows': -1,
+            'fixed_bid': 10,
+            'min_group_bid': 300,
+            'test_name': 'variable_user'
+        }
+        user_num_params = range(2000, 2500+1, 100)
+        user_timeslots_multiplier = 2
+        user_interest_multiplier = 1
+        configs = []
+        for num_user in user_num_params:
+            config = copy.copy(base_config)
+            config['total_users'] = num_user
+            config['user_interest_rows'] = num_user*user_interest_multiplier
+            config['user_timeslot_rows'] = num_user*user_timeslots_multiplier
+            configs.append(config)
+        out_file = base_config['test_name'] + '_results.csv'
+        for config in configs:
+            self._test_main(config, out_file)
+
+
+    def _test_fixture_files(self):
         base_path = 'test'
         configs = []
         for file in os.listdir(base_path):
-            config = {}
             with open(os.path.join(base_path,file), 'r') as fdr:
                 config = yaml.load(fdr)
             configs.append(config)
         for config in configs:
             self._test_main(config)
 
-    def _test_main(self, config):
+    def _test_main(self, config, out_file='test_result.csv'):
+        start_time = time.time()
         self._gen_input(config['total_users'],
                         config['user_interest_rows'],
                         config['total_titles'],
                         config['user_timeslot_rows'],
                         config['fixed_bid'])
+        start_read = time.time()
+        gen_input_duration = start_read - start_time
         self.read_input()
+        start_process = time.time()
+        read_duration = start_process - start_read
         min_group_bid = config['min_group_bid']
         calc = BidCalculator(min_group_bid, self._interest, self._timeslot,
                              log='.', log_level=logging.INFO)
         calc.main()
-        print('bids_found:',calc._bids_found)
-        print('user_booked:', calc._users_booked)
+        end_process = time.time()
+        process_duration = end_process - start_process
+        self._log_test_result(
+            start_time, end_process, end_process -start_time,
+                              gen_input_duration, read_duration, process_duration,
+                              config,
+                              calc._bids_found, calc._users_booked, calc._total_bids,
+                                out_file)
