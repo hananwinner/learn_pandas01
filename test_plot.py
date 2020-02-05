12d5ec65af2dff481f175c51c6fbff89595cb994
Hanan Wiener <hanan888@gmail.com>
2020-02-05 13:25:54 +0200

    ongoing

--- test_plot.py
+++ test_plot.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     df = pd.read_csv('variable_user-2_results.csv',
                      # index_col=['total_users'],
             dtype={
-                'test_name': 'object' ,
+                'test_name': 'object',
                 'start_time': 'float64',
                 'end_time': 'float64',
                 'total_duration': 'float64',
@@ -22,21 +22,9 @@ if __name__ == '__main__':
                 'result_num_group': 'int64', 'result_total_users': 'int64',
                 'result_total_money': 'int64'
             })
-    _columns = ['test_name',
-                    'gen_input_duration', 'read_input_duration',
-                    'process_duration',
-                'user_timeslot_rows',
-                    'result_num_group', 'result_total_users',
-                    'result_total_money',
-                    ]
-    # df = df.loc[:, _columns].groupby(by=["test_name"]).mean()
-    # print(df)
-    # df.so
-    ax = plt.gca()
-    df = df.sort_values(by='total_users')
-    # df.plot(kind='bar', x='total_users', y='process_duration', color='red',ax=ax)
 
+    # ax = plt.gca()
     df['user_percent'] = df['result_total_users'] / df['total_users'] * 100
-    df.plot(kind='bar', x='total_users', y='user_percent', color='blue', ax=ax)
-    plt.savefig('test/graphs/user_percent2.png')
-    # plt.show()
+    df.plot(kind='bar', x='total_users', y='user_percent', color='blue')
+    # plt.savefig('test/graphs/user_percent2.png')
+    plt.show()
