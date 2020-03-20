ace1835107a6018656ad967a21fa402d2cc5408b
Hanan Wiener <hanan888@gmail.com>
2020-03-20 08:53:31 +0200

    test titles data

---
+++ mymovie-service/test_data_scripts/fill_titles.py
@@ -0,0 +1,44 @@
+import boto3
+import uuid
+from datetime import datetime, timedelta
+
+
+def fill_titles(event, context):
+
+    num_titles = event['num_titles'] if 'num_titles' in event else 100
+    clear = event['clear'] if 'clear' in event else True
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('test-mymovie-titles')
+
+    if clear:
+        scan = table.scan(
+            ProjectionExpression='title_id'
+        )
+
+        with table.batch_writer() as batch:
+            for each in scan['Items']:
+                batch.delete_item(Key=each)
+
+    _from = datetime.strftime(datetime.now(), "%Y-%m-%d")
+    _to = datetime.strftime(datetime.now() + timedelta(days=14), "%Y-%m-%d")
+
+    with table.batch_writer() as batch:
+        for i in range(num_titles):
+            table.put_item(
+                Item={
+                    'title_id':
+                        str(uuid.uuid4())[:7]
+                    ,
+                    'title_name':
+                        str(uuid.uuid4())[:15]
+                    ,
+                    'from':
+                        _from
+                    ,
+                    'to': _to
+                },
+                ReturnValues='NONE',
+                ReturnConsumedCapacity='NONE',
+                # ExpressionAttributeNames={'#f': 'from', '#t': 'to'},
+            )
