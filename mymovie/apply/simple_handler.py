588a3344af8834649f6b642798dab40d0e8851d8
Hanan Wiener <hanan888@gmail.com>
2020-06-17 11:24:11 +0300

    apply: simple_handler : code review changes

--- mymovie/apply/simple_handler.py
+++ mymovie/apply/simple_handler.py
@@ -3,12 +3,14 @@ import json
 
 
 class SimpleSqsToDynDbHandler(object):
-    def __init__(self, queue_name, target_table_name, is_manual_delete=False):
-        sqs = boto3.resource('sqs')
-        self._queue = sqs.get_queue_by_name(QueueName=queue_name)
+    def __init__(self, queue_name, target_table_name,
+                 via_event_source_mapping=True):
+        self._wrapped_mode = via_event_source_mapping
+        if not self._wrapped_mode:
+            sqs = boto3.resource('sqs')
+            self._queue = sqs.get_queue_by_name(QueueName=queue_name)
         dynamodb = boto3.resource('dynamodb')
         self._target_table = dynamodb.Table(target_table_name)
-        self._manual_delete = is_manual_delete
 
     def _poll(self):
         messages = self._queue.receive_messages(
@@ -16,9 +18,11 @@ class SimpleSqsToDynDbHandler(object):
         )
         return messages
 
-    @staticmethod
-    def _get_data_dict(message):
-        return json.loads(message["body"])
+    def _get_data_dict(self, message):
+        if self._wrapped_mode:
+            return json.loads(message["body"])
+        else:
+            return json.loads(message.body)
 
     @staticmethod
     def _delete_message(message):
@@ -35,15 +39,17 @@ class SimpleSqsToDynDbHandler(object):
                 item = self._try_make_item(data)
                 if item is not None:
                     self._target_table.put_item(Item=item)
-                if self._manual_delete:
+                if not self._wrapped_mode:
                     self._delete_message(a_message)
             except OSError:
                 pass
             except ValueError:
-                if self._manual_delete:
+                if not self._wrapped_mode:
                     self._delete_message(a_message)
 
     def main(self):
+        if self._wrapped_mode:
+            raise NotImplementedError('dont run main in wrapped-mode, use process_messages')
         while True:
             messages = self._poll()
             self.process_messages(messages)
